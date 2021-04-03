2af6c7959f5b9faf2a36619c3d5089e69742d13c
Empyr3an <harshasomisetty7@gmail.com>
2021-04-02 21:58:00 -0400

    added methods for differences between who 2 accounts follow, and added method to follow everyone someone else follows

--- Artha/twitter.py
+++ Artha/twitter.py
@@ -1,22 +1,24 @@
 import requests
 from requests_oauthlib import OAuth1
-import itertools
+# import itertools
 import pymongo
 import urllib
 from . import mongo_config as c
-
-
-# import time
+import numpy as np
+import json
 
 
 class TwitterAPI:
 
-    def __init__(self, bearer_token, key=None, secret=None,
+    def __init__(self, username, bearer_token, key=None, secret=None,
                  token=None, token_secret=None):
 
         self.endpoint2 = 'https://api.twitter.com/2'
         self.endpoint1 = 'https://api.twitter.com/1.1'
         self.bearer = {"Authorization": "Bearer " + bearer_token}
+        self.username = username
+        self.id = requests.get(self.endpoint2+"/users/by/username/"+username,
+                               headers=self.bearer).json()["data"]["id"]
 
         if key and secret and token and token_secret:
             self.oauth = OAuth1(key, secret, token, token_secret)
@@ -48,29 +50,32 @@ class TwitterAPI:
         user_id = self.user_lookup(user_name)["id"]
         url = self.endpoint2 + "/users/" + user_id +\
                                "/following?max_results=1000"
+
+        data = []
         try:
             req = requests.get(url, headers=self.bearer)
-            yield req.json()["data"]
+            data = req.json()["data"]
         except Exception as e:
             raise e
 
-        while "next_token" in req["meta"]:
+        while "next_token" in req.json()["meta"]:
             try:
                 req = requests.get(url+"&pagination_token=" +
                                    req["meta"]["next_token"],
                                    headers=self.bearer)
-                yield req.json()["data"]
+                data[len(data)-1:len(data)-1] = req.json()["data"]
             except Exception as e:
                 raise e
 
+        return [str(i) for i in data]
+
     def get_following1(self, user_name):
-        # user_id = self.user_lookup(user_name)["id"]
-        url = self.endpoint1 + "/friends/ids.json?" +\
-                               "&count=5000" +\
-                               "&screen_name=" + user_name
+        url = self.endpoint1 + ("/friends/ids.json?" +
+                                "&count=5000" +
+                                "&screen_name=" + user_name)
         try:
             req = requests.get(url, auth=self.oauth)
-            yield req.json()["ids"]
+            data = req.json()["ids"]
         except Exception as e:
             raise e
 
@@ -79,27 +84,29 @@ class TwitterAPI:
                 req = requests.get(url+"&cursor=" +
                                    req.json()["next_cursor_str"],
                                    auth=self.oauth)
-                yield req.json()["ids"]
+                data[len(data)-1:len(data)-1] = req.json()["ids"]
             except Exception as e:
+                print(req.json())
                 raise e
 
-    def get_tweets(self, user_name, results=500,
+        return [str(i) for i in data]
+
+    def get_tweets(self, user_name, count=500,
                    start_date=None, tweet_fields=None):
-        if not start_date:
+        if not start_date:  # get tweets from earliest of history
             user_data = self.user_lookup(user_name, user_fields=["created_at"])
             start_date = user_data["created_at"]
 
-        url = self.endpoint + '/tweets/search/all?max_results=' +\
-                              str(results) + '&start_time=' + start_date +\
-                              '&query=from:' + user_name
+        if not tweet_fields:  # my default selected payload parameters
+            tweet_fields = ["created_at", "context_annotations", "entities",
+                            "in_reply_to_user_id", "referenced_tweets"
+                            ]
 
-        if tweet_fields:
-            url += "&tweet.fields=" + ",".join(tweet_fields)
-        else:
-            url += "&tweet.fields=" + ",".join([
-                    "created_at", "context_annotations", "entities",
-                    "in_reply_to_user_id", "referenced_tweets"
-                    ])
+        url = self.endpoint + ('/tweets/search/all?max_results=' + str(count) +
+                               '&start_time=' + start_date +
+                               '&query=from:' + user_name +
+                               '&tweet.fields=' + ','.join(tweet_fields))
+
         try:
             req = requests.get(url, headers=self.bearer)
             yield req.json()["data"]
@@ -115,6 +122,39 @@ class TwitterAPI:
             except Exception as e:
                 raise e
 
+    def update_with_new_followers(self, from_user):
+
+        to_follow = self.get_follow_differences(from_user, self.username)
+        blacklist = ['3393103900',
+                     '1356259499431129092',
+                     '1036115996',
+                     '1272699992575217664'
+                     ]
+        for follow in to_follow:
+            if follow not in blacklist:
+                try:
+                    self.follow_user_id(follow)
+                except Exception as e:
+                    raise(e)
+                else:
+                    print("followed", self.user_lookup(follow)["name"], follow)
+
+    # returns all user ids user1 follows that user2 does not
+    def get_follow_differences(self, user1, user2):
+        return np.setdiff1d(self.get_following1(user1),
+                            self.get_following1(user2))
+
+    def follow_user_id(self, user_id):
+        try:
+            requests.post(
+                self.endpoint2+"/users/"+self.id+"/following",
+                auth=self.oauth,
+                headers={"Content-type": "application/json"},
+                data=json.dumps({"target_user_id": user_id})
+            )
+        except Exception as e:
+            return e
+
     def rate_limits(self):
         return requests.get(self.endpoint1 +
                             "/application/rate_limit_status.json",
@@ -129,60 +169,71 @@ class TwitterAPI:
                             headers=self.bearer).status_code
 
 
+# class to manage reference data stored in mongodb
 class TMongo:
 
     username = urllib.parse.quote_plus(c.mongo_username)
     password = urllib.parse.quote_plus(c.mongo_password)
-    cluster = pymongo.MongoClient("mongodb+srv://{}:{}@cluster0.qpf0e.mongodb.net/ \
-                                   Artha?retryWrites=true&w=majority"
+    cluster = pymongo.MongoClient("mongodb+srv://{}:{}"
+                                  "@cluster0.qpf0e.mongodb.net"
+                                  "/Artha?retryWrites=true&w=majority"
                                   .format(username, password))
     db = cluster["Twitter"]
 
+    # add information about a user and all their following to mongodb database
     @classmethod
     def update_account_data(cls, twitter, username, following):
-        following.append(twitter.user_lookup(username)["id"])
-        url = "https://api.twitter.com/2/users?user.fields=\
-               created_at,\
-               description,\
-               entities,\
-               id,\
-               name,\
-               protected,\
-               url,\
-               username,\
-               verified,\
-               public_metrics\
-               &ids="
+        # adds desired
+        following.append(str(twitter.user_lookup(username)["id"]))
+
+        url = ('https://api.twitter.com/2/users?user.fields='
+               'created_at,'
+               'description,'
+               'entities,'
+               'id,'
+               'name,'
+               'protected,'
+               'url,'
+               'username,'
+               'verified,'
+               'public_metrics'
+               '&ids=')
 
         follow_data = []
         for i in range(0, int(len(following)/100)+1):
             req = requests.get(url+",".join(following[i*100:i*100 + 100]),
-                               headers=twitter.bearer)["data"]
-            follow_data[len(follow_data)-1:len(follow_data)-1] = req.json()
+                               headers=twitter.bearer).json()["data"]
+            follow_data[len(follow_data)-1:len(follow_data)-1] = req
 
-        cls.db["TwitterUsers"].insert_many([user for user in follow_data])
+        # return cls.db["TwitterUsers"].update_many(
+        #                           [user for user in follow_data])
+        # print("updated mongo")
 
 
 class TSQLite:
 
     @classmethod
     def create_follow_table(cls, conn, twitter, username):
-        gen = twitter.get_following1(username)  # generator of follows
-        following = list(map(str, list(itertools.chain.from_iterable(gen))))
-
+        print('creating table for ', username)
+        following = [str(id) for id in twitter.get_following1(username)]
+        # following_names = 
         try:
             with conn:
+                if "following" in cls.current_tables(conn):
+                    conn.cursor().execute("DROP TABLE following")
+
                 conn.cursor().execute("\
                     CREATE TABLE following\
                     (account_id str)")
-                for user_info in following:
+
+                for user_id in following:
                     conn.cursor().execute("INSERT INTO following\
-                                          VALUES(:id)", [user_info])
+                                           VALUES(:account_id)", [user_id])
         except Exception as e:
             print(following[:10])
             raise e
-
-        TMongo.update_account_data(twitter, username, following)
+        # print("updating mongo")
+        # TMongo.update_account_data(twitter, username, following)
 
     @classmethod
     def drop_table(cls, conn, name):
@@ -199,8 +250,4 @@ class TSQLite:
                   """).fetchall()
                   if v[0] != "sqlite_sequence"]
 
-        return [(v, len(conn.cursor().execute("SELECT id \
-                                               FROM " + v).fetchall()))
-                for v in tables]
-
-        # TODO implement autofollow
+        return tables
