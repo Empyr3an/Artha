28162ab43f697ef82cbd8f007367784a0a623ed9
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-04-10 22:09:02 -0400

    finsihed inserting into neo4j from sqlite, but insert one by one is very slow

--- Artha/neo.py
+++ Artha/neo.py
@@ -9,23 +9,27 @@ class Neo:
         self.driver = GraphDatabase.driver(uri, auth=(username, password))
         self.session = self.driver.session()
 
+    # TODO only add if node doensn't already exist
     def create_node(self, params):
         self.session.run("CREATE(p:Person $param)", param=params)
 
-    def create_relation(session, from_id, to_id, type="Follows"):
-        result = session.run("match (n:Person {id: $from_id}), "
-                             "(m:Person {id: $to_id})"
-                             f"merge (n)-[:{type}]- (m)",
-                             "return n.username, m.username",
-                             from_id=from_id, to_id=to_id)
+    # TODO only add if relation doensn't already exist
+    def create_relation(self, from_username, to_username):
+        result = self.session.run("MATCH (n:Person), (m:Person) "
+                                  "WHERE n.username = $from_username "
+                                  "AND m.username = $to_username "
+                                  "MERGE (n)-[:Follows]->(m)"
+                                  "RETURN n.username, m.username",
+                                  from_username=from_username,
+                                  to_username=to_username)
 
         return [record for record in result]
 
-    def check_relation(session, from_id, to_id):
-        result = session.run("MATCH (n:Person {id: $from_id})-[r]- "
-                             "(m:Person {id: $to_id})"
-                             "RETURN n,type(r),m",
-                             from_id=from_id, to_id=to_id)
+    def check_relation(self, from_id, to_id):
+        result = self.session.run("MATCH (n:Person {id: $from_id})-[r]-> "
+                                  "(m:Person {id: $to_id})"
+                                  "RETURN n,type(r),m",
+                                  from_id=from_id, to_id=to_id)
         return [record for record in result]
 
     def clear_db(self):
@@ -37,16 +41,18 @@ class Neo:
         return [record["n"] for record in nodes]
 
     def get_relations(self):
-        relations = self.session.run("MATCH (n)-[r]- (m) RETURN n, r, m")
+        relations = self.session.run("MATCH (n)-[r]-> (m) RETURN n, r, m")
         return [record["r"] for record in relations]
 
     def print_nodes(self):
         for rec in self.get_nodes():
-            print(rec["username"])
+            print(rec["username"], rec["id"])
 
     def print_relations(self):
         for rec in self.get_relations():
             print(rec.start_node["username"],
+                  rec.start_node["id"],
                   rec.type,
-                  rec.end_node["username"]
+                  rec.end_node["username"],
+                  rec.end_node["id"]
                   )
