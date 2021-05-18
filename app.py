638ad408b06c5537e44da6da32c62136161cc57c
Harsha Somisetty <harshasomisetty7@gmail.com>
2021-05-18 01:01:07 -0400

    started annotations of indicators

--- app.py
+++ app.py
@@ -15,23 +15,35 @@ df = load_klines("ETHBTC")
 
 
 ichimoku(df)
-d = df[-250:]
+d = df[-550:]
 
 fig = setup_ichi_graph(d, ticker, time_frame, cloud=True, tkc=True)
 
 # fig.update_layout(
-#     title='The Great Recession',
-#     yaxis_title='AAPL Stock',
 #     shapes = [dict(
-#         x0='2016-12-09', x1='2016-12-09', y0=0, y1=1, xref='x', yref='paper',
+#         x0='2021-04-25', x1='2021-04-25', y0=0, y1=1, xref='x', yref='paper',
 #         line_width=2)],
 #     annotations=[dict(
-#         x='2016-12-09', y=0.05, xref='x', yref='paper',
+#         x='2021-04-25', y=0.05, xref='x', yref='paper',
 #         showarrow=False, xanchor='left', text='Increase Period Begins')]
 # )
 
+bull_tks = [d.bull_tk_cross.index[i] for i in range(len(d)) if d.bull_tk_cross.values[i] == 1]
+for date_str in bull_tks:
+    fig.update_layout(
+        shapes=[dict(x0=date_str, x1=date_str, y0=0, y1=1, xref='x', yref='paper',
+                line_width=1)],
+        annotations=[dict(
+            x=date_str, y=0.05, xref='x', yref='paper',
+            showarrow=False, xanchor='left', text='Bull TK cross')]
+)
 
-
+# TODO PROPER INDICATORS annotation
+# TODO better graph formatting, (fit screen, be able to scroll)
+# TODO confluence of ichimoku indicators
+# TODO rsi confluence
+# TODO volume confluence
+# TODO social media confluence
 
 app = dash.Dash()
 app.layout = html.Div([
